b670ce1143b8a312eddc9c41948061f17a781697
lpshkn <kofffsh@gmail.com>
2020-04-09 23:34:42 +0300

    Add error class for emptiness of file and raise errors
    Now EmptyFileError raises if specified file is empty. To check this use os.stat class.
    If specified count of words is 0, raise ValueError, because it's incorrect and not intuitively understandable case.

--- analyzer/load_data.py
+++ analyzer/load_data.py
@@ -4,6 +4,7 @@ This module implements loading words from the file
 
 import codecs
 from random import sample
+from os import stat
 
 
 def load_words(words_filename, count=None):
@@ -13,7 +14,21 @@ def load_words(words_filename, count=None):
     :return: list(str) - list of loaded words
     """
 
+    if stat(words_filename).st_size == 0:
+        raise EmptyFileError()
+
+    if count == 0:
+        raise ValueError("Count can't be 0")
+
     with codecs.open(words_filename, 'r', 'ANSI') as f:
         data = f.read().splitlines()
 
-    return sample(data, count) if count else data
+    return sample(data, count) if count is not None else data
+
+
+class EmptyFileError(Exception):
+    """
+    The error will be raised when empty file is passed to input
+    """
+    def __init__(self):
+        self.text = "The specified file is empty!"
