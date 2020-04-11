0f7182b7ce60b1c35a6e70935ad0d501f74ed21c
lpshkn <kofffsh@gmail.com>
2020-04-12 00:40:10 +0300

    Add description and comment to leet function

--- analyzer/methods.py
+++ analyzer/methods.py
@@ -1,7 +1,20 @@
+"""
+This module contains functions for processing string, clearing from incorrect symbols or another actions with it
+"""
+
 import re
 
 
 def leet_transform(word: str, indices: list) -> str:
+    """
+    This functions clears the passed word from digits, service symbols or any incorrect characters.
+    Also it corrects "leet" characters if their indices were passed
+    :param word: the word you need to correct
+    :param indices: indices of incorrect characters that will be corrected
+    :return: corrected word
+    """
+
+    # "Leet" characters that need to be replaced
     comparison = {
         '$': 's',
         '1': 'i',
@@ -11,11 +24,14 @@ def leet_transform(word: str, indices: list) -> str:
     }
 
     for index in indices:
+        # Get corrected symbol
         symbol = comparison.get(word[index])
 
         if not symbol:
             continue
 
+        # Build corrected word
         word = word[:index] + symbol + word[index + 1:]
 
+    # Remove all incorrect symbols
     return re.sub(r'[\W\d\s_]+', r'', word)
