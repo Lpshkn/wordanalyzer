9d9c24d2ec78b6380d208b86548e9d1047576135
lpshkn <kofffsh@gmail.com>
2020-04-25 17:44:48 +0300

    Add the realisation of two extra options to have possibility get total cost and get cleared words

--- analyzer/main.py
+++ analyzer/main.py
@@ -6,30 +6,47 @@ from analyzer.word_analyzer import WordAnalyzer
 
 def main():
     args = arguments_parser().parse_args(sys.argv[1:])
-    words = load_words(args.source, args.count, args.encoding)
+
+    if args.words:
+        words = args.words
+    else:
+        words = load_words(args.source, args.count, args.encoding)
     frequency_words = load_words(args.frequency)
 
     word_analyzer = WordAnalyzer(frequency_words, args.tree, number_similar_words=args.similar_words,
                                  distance=args.distance, threshold=args.threshold,
                                  number_of_corrected_words=args.number_corrected)
 
-    with open(args.destination, 'w') as file:
-        print("Correcting words is beginning...")
+    if args.clear_word:
+        print("Clearing words is beginning...")
         for word in words:
-            new_words = word_analyzer.get_correct_words(word)
-            if args.verbose:
-                print('ORIGINAL: ', word, ' CORRECTED: ', new_words)
-            if new_words:
-                file.writelines('\n'.join(new_words))
-                file.write('\n')
+            new_word = word_analyzer.get_clear_word(word)
+            print('ORIGINAL: ', word, ' CLEARED: ', new_word)
 
-    # Remove duplicates and make all words lower
-    with open(args.destination, 'r+') as file:
-        words = set(word.lower() for word in file.readlines())
-        file.truncate(0)
-        file.writelines(words)
+    elif args.total_cost:
+        print("Counting total cost is beginning...")
+        for word in words:
+            total_cost = word_analyzer.get_total_cost(word)
+            print('WORD: ', word, ' TOTAL COST: ', total_cost)
 
-    print("New corrected words were saved successfully")
+    else:
+        with open(args.destination, 'w') as file:
+            print("Correcting words is beginning...")
+            for word in words:
+                new_words = word_analyzer.get_correct_words(word)
+                if args.verbose:
+                    print('ORIGINAL: ', word, ' CORRECTED: ', new_words)
+                if new_words:
+                    file.writelines('\n'.join(new_words))
+                    file.write('\n')
+
+        # Remove duplicates and make all words lower
+        with open(args.destination, 'r+') as file:
+            words = set(word.lower() for word in file.readlines())
+            file.truncate(0)
+            file.writelines(words)
+
+        print("New corrected words were saved successfully")
 
 
 if __name__ == '__main__':
