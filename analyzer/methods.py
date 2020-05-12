0676f3d53ede851e05514f44e30d99bae471b9e2
lpshkn <kofffsh@gmail.com>
2020-05-12 21:44:24 +0300

    Add a function to process a list of words and print results to the command line
    This function may save results or/and pass them to stdout

--- analyzer/methods.py
+++ analyzer/methods.py
@@ -3,7 +3,7 @@ This module contains functions for processing string, clearing from incorrect sy
 """
 
 import re
-from itertools import combinations
+from collections.abc import Iterable
 
 
 def leet_transform(word: str, indices: tuple) -> str:
@@ -74,3 +74,55 @@ def factorize(number: int) -> list:
             divisors.append(divisor)
         divisor += 1
     return divisors
+
+
+def process_words(function, words: list, destination: str = None, prefix: str = None, postfix: str = None,
+                  verbose_pattern: str = None, file_pattern: str = None, duplicate: bool = False):
+    """
+    This function applies the passed function to the list of words, then processes these words and prints all results
+    in the command line or/and saves to the file if necessary. All preferences are set by the passed arguments.
+
+    :param function: a function will be used to a list of words
+    :param words: a list of words that you need to process
+    :param destination: specify the filename, if you need to save results to the file
+    :param prefix: this message will be printed before processing
+    :param postfix: this message will be printed after successful processing
+    :param verbose_pattern: words will be inserted into this pattern and this string will be passed into stdout.
+            If it isn't specified, the string won't be printed
+    :param file_pattern: words will be inserted into this pattern and this string will be saved into the file.
+            If it isn't specified, only processed words will be saved.
+    :param duplicate: if it's specified and it's True, duplicates won't be deleted
+    """
+    if prefix:
+        print(prefix)
+
+    file = open(destination, 'w') if destination else None
+    for word in words:
+        processed = function(word)
+
+        # Convert to one form
+        if not isinstance(processed, Iterable):
+            processed = [processed]
+
+        for processed_word in processed:
+            if verbose_pattern:
+                print(verbose_pattern.format(word, processed_word))
+            if file:
+                if file_pattern:
+                    file.write(file_pattern.format(word, processed_word))
+                else:
+                    file.write(processed_word)
+                file.write('\n')
+                file.flush()
+
+    # Delete duplicates
+    if file:
+        file.close()
+        if not duplicate and destination:
+            with open(destination, 'r+') as file:
+                words = set(word.lower() for word in file.readlines())
+                file.truncate(0)
+                file.writelines(words)
+
+    if postfix:
+        print(postfix)
