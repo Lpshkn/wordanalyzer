f71b1a40e203ce208be498c66f82a1f41464c345
lpshkn <kofffsh@gmail.com>
2020-05-16 22:35:37 +0300

    Change the function of printing words, add function to delete duplicates in a file
    Now this function doesn't process a list of words and just prints already ready results at the desired format.

--- analyzer/methods.py
+++ analyzer/methods.py
@@ -3,9 +3,9 @@ This module contains functions for processing string, clearing from incorrect sy
 """
 
 import re
+import collections
+import analyzer.configurator as cfg
 from collections.abc import Iterable
-from nltk.stem import SnowballStemmer
-from analyzer.text_splitter import TextSplitter
 
 
 def leet_transform(word: str, indices: tuple) -> str:
@@ -78,56 +78,45 @@ def factorize(number: int) -> list:
     return divisors
 
 
-def process_words(function, words: list, destination: str = None, prefix: str = None, postfix: str = None,
-                  verbose_pattern: str = None, file_pattern: str = None, duplicate: bool = False):
+def process_words(word: str, pattern: str, args: list, file, verbose: bool = False):
     """
     This function applies the passed function to the list of words, then processes these words and prints all results
     in the command line or/and saves to the file if necessary. All preferences are set by the passed arguments.
-
-    :param function: a function will be used to a list of words
-    :param words: a list of words that you need to process
-    :param destination: specify the filename, if you need to save results to the file
-    :param prefix: this message will be printed before processing
-    :param postfix: this message will be printed after successful processing
-    :param verbose_pattern: words will be inserted into this pattern and this string will be passed into stdout.
-            If it isn't specified, the string won't be printed
-    :param file_pattern: words will be inserted into this pattern and this string will be saved into the file.
-            If it isn't specified, only processed words will be saved.
-    :param duplicate: if it's specified and it's True, duplicates won't be deleted
     """
-    processed_words = []
-
-    if prefix:
-        print(prefix)
-
-    file = open(destination, 'w') if destination else None
-    for word in words:
-        processed = function(word)
-
-        # Convert to one form
-        if not isinstance(processed, Iterable):
-            processed = [processed]
-
-        processed_words.extend(processed)
-        for processed_word in processed:
-            if verbose_pattern:
-                print(verbose_pattern.format(word, processed_word))
-            if file:
-                if file_pattern:
-                    file.write(file_pattern.format(word, processed_word))
+    if verbose:
+        if len(args) == 1:
+            for arg in args:
+                if isinstance(arg, Iterable) and not isinstance(arg, str):
+                    for arg_word in arg:
+                        file.write(pattern.format(word, arg_word))
                 else:
-                    file.write(processed_word)
-                file.write('\n')
-                file.flush()
+                    file.write(pattern.format(word, arg))
+        else:
+            args = [arg if arg else 'NONE' for arg in args]
+            file.write(pattern.format(word, *args))
+    else:
+        if len(args) == 1:
+            for arg in args:
+                if not arg:
+                    break
 
-    # Delete duplicates
-    if file:
-        file.close()
-        if not duplicate and destination:
-            with open(destination, 'r+') as file:
-                words = set(word.lower() for word in file.readlines())
-                file.truncate(0)
-                file.writelines(words)
+                if isinstance(arg, Iterable) and not isinstance(arg, str):
+                    for arg_word in arg:
+                        file.write(pattern.format(arg_word))
+                else:
+                    file.write(pattern.format(arg))
+        else:
+            args = [arg if arg else 'NONE' for arg in args]
+            file.write(pattern.format(*args))
+    file.flush()
+
+
+def delete_duplicates(filename: str):
+    if filename:
+        with open(filename, 'r+') as file:
+            words = set(word.lower() for word in file.readlines())
+            file.truncate(0)
+            file.writelines(words)
 
     if postfix:
         print(postfix)
