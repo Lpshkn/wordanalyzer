708520b6bb76e3ddf3114495144e49d3b903f603
lpshkn <kofffsh@gmail.com>
2020-05-20 01:53:55 +0300

    Add the time measurement for the analyze method, edit the dataset
    This timer will be printed in the end of processing.
    The dataset contains a lot of garbage and it's necessary to remove it.

--- analyzer/main.py
+++ analyzer/main.py
@@ -1,12 +1,15 @@
 import sys
 from analyzer.lib.configurator import Configurator
 from analyzer.lib.word_analyzer import WordAnalyzer
+from datetime import datetime
 
 
 def main():
     configurator = Configurator(sys.argv[1:])
     analyzer = WordAnalyzer.build(configurator)
+    time = datetime.now()
     analyzer.analyze()
+    print("Working time: ", datetime.now() - time)
 
 
 if __name__ == '__main__':
